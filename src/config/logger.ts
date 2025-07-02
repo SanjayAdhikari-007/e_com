b58117b9572758dcc44695a37f0ca5cb9c58e928
SanjayAdhikari-007 <freakin.sanjay07@gmail.com>
2025-07-02 16:53:39 +0545

    Updated

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,18 +1,39 @@
 import { createLogger, format, transports } from 'winston';
 
+const { combine, timestamp, printf, colorize, errors } = format;
+
+
+const logFormat = printf(({ level, message, timestamp, stack }) => {
+  return `${timestamp} ${level}: ${stack || message}`;
+});
+
 const logger = createLogger({
-  level: 'info', // Set default log level
-  format: format.combine(
-    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    format.errors({ stack: true }), // Include stack trace for errors
-    format.splat(),
-    format.json() // Output logs in JSON format
+  level: 'info', 
+  format: combine(
+    errors({ stack: true }),
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    logFormat
   ),
   transports: [
-    new transports.Console(), // Log to console
-    new transports.File({ filename: 'logs/error.log', level: 'error' }), // Log errors to error.log
-    new transports.File({ filename: 'logs/combined.log' }) // Log all levels to combined.log
-  ]
+    new transports.Console({
+      format: combine(
+        colorize(), 
+        logFormat
+      ),
+      level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
+    }),
+  ],
+  exitOnError: false,
 });
+
+
+logger.exceptions.handle(
+  new transports.Console({
+    format: combine(
+      colorize(),
+      logFormat
+    )
+  })
+);
 
 export default logger;
